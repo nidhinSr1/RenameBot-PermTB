9c55acbcdb72b510b50a6793c7249960ef2209bb
cinemafactoryt <68299226+cinemafactoryt@users.noreply.github.com>
2020-07-16 13:52:36 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -20,7 +20,7 @@ class Translation(object):
     CUSTOM_CAPTION_UL_FILE = "*caption=file_name*"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    HELP_USER = """Sorry.. This Bot Can Only Be Used in [Cinema Factory](https://t.me/cf_chat) Group Admins"""
+    HELP_USER = """Sorry.. This Bot Can Only Be Used in [Cinema Factory](https://t.me/cf_chat)"Group Admins"""
     
 
 
